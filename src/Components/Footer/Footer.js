bc47b31454867ceecc7a0d1036931daa4fd00c8c
Aribaskar-jb <j.b.aribaskar1515@gmail.com>
2026-02-08 09:50:19 +0530

    inital commit

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import "./Footer.css";
+export default function Footer() {
+  return (
+    <div className="App-footer">
+      <p>This is Footer Component</p>
+    </div>
+  );
+}
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,8 @@
+import "./Header.css";
+export default function Header() {
+  return (
+    <div className="App-header">
+      <p>This is Header Component</p>
+    </div>
+  );
+}
